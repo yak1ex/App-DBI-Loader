15231765433c36d1928587c4089eecba947a281e
Yasutaka ATARASHI <yakex@cpan.org>
2013-10-30 19:34:51 +0900

    Initial working version.

---
+++ README.md
@@ -0,0 +1,34 @@
+# NAME
+
+App::DBI::Loader - A tiny script to load CSV/TSV contents into a database table via DBI
+
+# VERSION
+
+version v0.0.0
+
+# SYNOPSIS
+
+    App::DBI::Loader->run(@ARGV);
+
+# DESCRIPTION
+
+This is an implementation module for a tiny script to load CSV/TSV contents into a database table via DBI.
+
+# METHODS
+
+## `run(@arg)`
+
+Process arguments. Typically, `@ARGV` is passed. For details, see [dbiloader](http://search.cpan.org/perldoc?dbiloader).
+
+- [dbiloader](http://search.cpan.org/perldoc?dbiloader)
+
+# AUTHOR
+
+Yasutaka ATARASHI <yakex@cpan.org>
+
+# COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2013 by Yasutaka ATARASHI.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
--- lib/App/DBI/Loader.pm
+++ lib/App/DBI/Loader.pm
@@ -3,14 +3,68 @@ package App::DBI::Loader;
 use strict;
 use warnings;
 
-# ABSTRACT:
+# ABSTRACT: A tiny script to load CSV/TSV contents into a database table via DBI
 # VERSION
+
+use Getopt::Std;
+use Getopt::Config::FromPod;
+use Pod::Usage;
+
+use DBI;
+
+sub run
+{
+	shift if @_ && eval { $_[0]->isa(__PACKAGE__) };
+	local (@ARGV) = @_;
+
+	my %opts;
+	getopts(Getopt::Config::FromPod->string, \%opts);
+	pod2usage(-verbose => 2) if exists $opts{h};
+	pod2usage(-msg => 'At least 3 arguments MUST be specified', -verbose => 0, -exitval => 1) if @ARGV < 3;
+
+	# FIXME: Should avoid stringy eval
+	$opts{t} ||= '';
+	my $sep = eval("\"$opts{t}\"") || ','; ## no critic (ProhibitStringyEval)
+
+	my $dbstr = shift @ARGV;
+	my $table = shift @ARGV;
+
+	my $dbh = DBI->connect($dbstr) or die;
+	if(exists $opts{i}) {
+		$dbh->do("DROP TABLE IF EXISTS $table");
+		$dbh->do("CREATE TABLE $table $opts{i}");
+	}
+	my $sth;
+
+	$dbh->begin_work;
+	while(my $file = shift @ARGV) {
+		open my $fh, '<', $file or die;
+		while(<$fh>) {
+			s/[\r\n]+$//;
+			my (@t) = $sep ? split /$sep/ : $_;
+			$sth ||= $dbh->prepare('INSERT INTO '.$table.' VALUES ('.join(',', '?'x @t).')');
+			$sth->execute(@t);
+		}
+	}
+	$dbh->commit;
+}
 
 1;
 __END__
 
 =head1 SYNOPSIS
 
+  App::DBI::Loader->run(@ARGV);
+
 =head1 DESCRIPTION
+
+This is an implementation module for a tiny script to load CSV/TSV contents into a database table via DBI.
+
+=method C<run(@arg)>
+
+Process arguments. Typically, C<@ARGV> is passed. For details, see L<dbiloader>.
+
+=for :list
+* L<dbiloader>
 
 =cut
